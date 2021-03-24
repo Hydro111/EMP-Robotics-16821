49f9d1cd53db94d9595bdcb4b59a27279d0170e9
Hydro111 <67488614+Hydro111@users.noreply.github.com>
2021-03-24 11:22:39 -0600

    update drive.java
    Separate move and turn
    TODO: insert code to set angel of wheels

--- drive.java
+++ drive.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import Math;
 
 @TeleOp(name = "Autoanumos (Blocks to Java)", group = "")
 public class Autoanumos extends LinearOpMode {
@@ -25,20 +26,53 @@ public class Autoanumos extends LinearOpMode {
     motor1AsDcMotor = hardwareMap.get(DcMotor.class, "motor1AsDcMotor");
     motor0AsDcMotor = hardwareMap.get(DcMotor.class, "motor0AsDcMotor");
     motor3AsDcMotor = hardwareMap.get(DcMotor.class, "motor3AsDcMotor");
+    
+    motor1A = motor0AsDcMotor;
+    motor2A = motor1AsDcMotor;
+    motor1B = motor2AsDcMotor;
+    motor2B = motor3AsDcMotor;
+    // 1A 2A
+    // 1B 2B
   }
 }
 
 
 public class Drive {
+  
+  public float distCenter = 14;
+  
   static void stop() {
-    motor2AsDcMotor.setPower(0.0);
-    motor1AsDcMotor.setPower(0.0);
-    motor0AsDcMotor.setPower(0.0);
-    motor3AsDcMotor.setPower(0.0);
+    motor1A.setPower(0.0);
+    motor2A.setPower(0.0);
+    motor1B.setPower(0.0);
+    motor2B.setPower(0.0);
+    //Set wheels to forward position
   }
   
-  static void forTime(speed, degps, time){
-    pass
-    //Unfinished
+  static void moveTurn(speed, turn, strafe){
+    public float s2 = (2*Math.PI*distCenter*turn/360 + 2*speed) / 2;
+    public float s1 = 2*speed - s2;
+    motor1A.setPower(s2);
+    motor2A.setPower(s1);
+    motor1B.setPower(s2);
+    motor2B.setPower(s1);
+    
+    //TODO: add strafe capability?
+  }
+  
+  static void turn(power){
+    //put wheels in rotation posittion
+    motor1A.setPower(power);
+    motor2A.setPower(power);
+    motor1B.setPower(power);
+    motor2B.setPower(power);
+  }
+  
+  static void move(power, strafe){
+    //set wheels to angle %strafe%
+    motor1A.setPower(power);
+    motor2A.setPower(power);
+    motor1B.setPower(power);
+    motor2B.setPower(power);
   }
 }
